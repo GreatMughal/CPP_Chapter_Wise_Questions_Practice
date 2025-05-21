7f276fe4fe67bf25c0cc6441285a982b5e49858b
Anas Mughal <147911767+GreatMughal@users.noreply.github.com>
2025-05-21 06:03:16 +0500

    Update 01.cpp

--- Chapter_07 Arrays/01.cpp
+++ Chapter_07 Arrays/01.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-
-    int myArr[10];
+int main() {
+    int arr[10];
     int count = 0;
-    for(int i = 0; i < 10; i++){
-        cout << "Enter integer: " << i + 1 << " ";
-        cin >> myArr[i];
-        if(myArr[i] % 2 != 0){
+
+    cout << "Enter 10 integers:\n";
+    for (int i = 0; i < 10; ++i) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        int n = arr[i];
+        bool isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int j = 2; j * j <= n; ++j) {
+                if (n % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
             count++;
         }
     }
 
-    cout << "Total Number of primes are: " << count << endl;
-
+    cout << "Total number of prime numbers in the array: " << count << endl;
 
     return 0;
 }
